2aff835cdd1fe375816a72eb831a7a1fd050b6df
Mallory Wittwer <mallory.wittwer@epfl.ch>
2025-03-27 16:12:47 +0100

    Update project

--- tests/test_run_algorithm.py
+++ tests/test_run_algorithm.py
@@ -50,7 +50,7 @@ def run_algorithm_server_side():
             assert isinstance(
                 data, np.ndarray
             ), "Algorithm did not output a Numpy array."
-        elif data_type == "labels":
+        elif data_type == "mask":
             assert isinstance(
                 data, np.ndarray
             ), "Algorithm did not output a Numpy array."
